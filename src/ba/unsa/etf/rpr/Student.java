bd29514ff67a2bc6d9a874f674d740fe973d237c
jusufdelalic <jdelalic1@etf.unsa.ba>
2018-11-23 22:12:24 +0100

    Added getters and setters in class Student

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -6,13 +6,38 @@ public class Student {
     private String prezime;
     private int brojIndeksa;
 
-    private int godinaStudija;
-    private int ciklusStudija;
+    public int getBrojIndeksa() {
+        return brojIndeksa;
+    }
 
+    public void setBrojIndeksa(int brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
 
-    public Student(String ime, String prezime, int brojIndeksa) {}
+    public String getIme() {
+        return ime;
+    }
 
-    public void ispisiPredmeteKojeSlusa() {}
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Student(String ime, String prezime, int brojIndeksa) {
+
+        this.setIme(ime);
+        this.setPrezime(prezime);
+        this.setBrojIndeksa(brojIndeksa);
+
+    }
+
 
 
 
